3f3578b2d0d99188aba4f00e8e1e6ed882c3e813
akitouno <piinball1973@gmail.com>
2022-12-09 17:24:54 +0900

    plot を白黒で行うように修正

--- gender/missforest.py
+++ gender/missforest.py
@@ -21,7 +21,6 @@ df = d.iloc[:, 1:]
 df = df.replace(np.inf, np.nan)
 df = df.drop('地', axis=1)
 df.to_csv('genderraw.csv', encoding='Shift-JIS')
-print(df)
 
 # NaNの個数を数え（→ プロットの欠損値%に使用）、降順にprint
 # すべてNaNの列はない
@@ -35,7 +34,6 @@ sys.modules['sklearn.neighbors.base'] = sklearn.neighbors._base
 # 欠損値への代入を行う部分(x)のみ抜き出し
 x = df.iloc[:, 2:]
 yearloc = df.iloc[:, :2]
-print(yearloc)
 
 # imputation
 # missforestを行った結果をdataframeにする過程で、一旦txtを挟む必要があった
@@ -47,30 +45,24 @@ x1 = imputer.fit_transform(x)
 np.savetxt('gendermiss.txt', x1, delimiter=',')
 x1 = np.loadtxt("gendermiss.txt", delimiter=',')
 x1_df = pd.DataFrame(x1)
-print(x1_df)
 df_miss = pd.concat([yearloc, x1_df], axis=1)
 df_miss.columns = df.columns
 df_miss.to_csv('gendermissingforest.csv', encoding='Shift-JIS')
-print(df_miss)
 
 # plot
 # 結果はcurrent directory 内に保存される
-print(df)
-print(df_miss)
 d_nan = d.isnull().sum()
-print(df_nan)
-
 df_plot = pd.concat([df, df_miss.iloc[:, 2:]], axis=1)
 df_plot.columns = range(len(df_plot.columns))
-print(df_plot)
 
 for i in range(0, 199, 1):
     d1 = df_plot.iloc[:, [0, 1, i + 2, i + 202]]
-    print(d1)
     fig, ax = plt.subplots(sharex='all')
     for key, data in d1.groupby(d1.columns[1]):
-        ax.scatter(data[d1.columns[0]], data[d1.columns[3]], c='lightgray', alpha=0.5, lw=0.5)
-        ax.scatter(data[d1.columns[0]], data[d1.columns[2]], c='red', alpha=0.5, lw=0.5)
-        ax.set_title(f'{df.columns[i + 3]}女男比 miss（欠損値{df_nan[i + 3] / 2162 * 100:.1f}%）')
-        fig.savefig(f'fmratio_miss{df.columns[i + 3]}.png')
-        plt.close(fig)
+        ax.scatter(data[d1.columns[0]], data[d1.columns[3]], c='white', edgecolors='black', alpha=1, lw=0.5)
+        ax.scatter(data[d1.columns[0]], data[d1.columns[2]], c='black', edgecolors='black',  alpha=1, lw=0.5)
+        ax.set_title(f'{df.columns[i + 3]}Female-Male ratio (missing value: {df_nan[i + 3] / 2162 * 100:.1f}%)')
+    # ここのインデント位置注意（for文が回りきらないまま図として保存されないように！）
+    fig.savefig(f'fmratio_miss{df.columns[i + 3]}.png')
+    plt.close(fig)
+
