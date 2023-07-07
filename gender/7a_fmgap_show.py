f550cec56680b458883aef048d9895de55eedf8c
sawaiyutaka <yusawai.tky@gmail.com>
2023-07-08 00:16:14 +0900

    男女別の発症率の描画

--- gender/7a_fmgap_show.py
+++ gender/7a_fmgap_show.py
@@ -14,19 +14,52 @@ from windi_library import GBD_japanmap_merge
 
 df = pd.read_csv("IHME-GBD_2019_DATA_47P_15-39.csv", delimiter=",")
 # cause列==Schizophrenia かつ metric列==Rateの行を抜き出し
-df=df[(df['cause']=='Schizophrenia')&(df['metric']=='Rate')]
+df = df[(df['cause'] == 'Idiopathic epilepsy') & (df['metric'] == 'Rate')]
 # 必要な列のみ残す
 df = df.loc[:, ['year', 'location', 'sex', 'val']]
 # 都道府県のID、ローマ字表記、漢字表記を並べたデータフレームに
 df = GBD_japanmap_merge(df)
 # 年度が1995-2015のデータのみ選択
-df = df [(1995<=df['year'])&(df['year']<=2015)]
+df = df[(1990 <= df['year']) & (df['year'] <= 2020)]
 
 # pivot 関数で男女それぞれのvalのコラム　→　FM ratio計算
-df=df.pivot(index=['year', 'ID', 'location', 'kanji'], columns='sex', values='val')
-df['fmratio']=df['Female']/df['Male']
-df=df.reset_index()
+df = df.pivot(index=['year', 'ID', 'location', 'kanji'], columns='sex', values='val')
+df['fmratio'] = df['Female'] / df['Male']
+df = df.reset_index()
 print(df)
+
+
+# for文で男女、都道府県ごとに線形回帰
+result = []
+ncol, nrow = 6, 8
+fig = plt.figure(figsize=(8, 8))
+
+for i in range(1, 48):
+    d1 = df[df['ID'] == i]
+    # print(d1)
+    model_lr = LinearRegression()
+    x = d1['year']
+    X = np.array(x).reshape(-1, 1)
+    y = d1['Male']
+    Y = np.array(y).reshape(-1, 1)
+    z = d1['Female']
+    Z = np.array(z).reshape(-1, 1)
+    mlr = model_lr.fit(X, Y)
+    mcoef = mlr.coef_[0, 0]  # 数値で入れるため、行列を指定
+    ax = plt.subplot2grid((nrow, ncol), (i // ncol, i % ncol))
+    ax = plt.plot(X, Y, 'o', color='blue')
+    ax = plt.plot(X, mlr.predict(X), color='black')
+    model_lr = LinearRegression()
+    flr = model_lr.fit(X, Z)
+    fcoef = flr.coef_[0, 0]  # 数値で入れるため、行列を指定
+    ax = plt.plot(X, Z, 'o', color='red')
+    ax = plt.plot(X, flr.predict(X), color='black')
+    plt.title(d1['location'].iloc[1])
+    plt.tight_layout()
+    result.append([d1['ID'].iloc[1], mcoef, fcoef])  # japanmapに渡すため、都道府県IDと並べる
+
+# plt.savefig(r"C:\Users\piinb\Documents\DR\WIND\socialfactor_regression.png")
+plt.show()
 
 # for文で男女、都道府県ごとに線形回帰
 result = []
@@ -62,7 +95,7 @@ mean = df_result['fmratio_regression'].mean()
 std = df_result['fmratio_regression'].std()
 df_result["value_map"] = (df_result['fmratio_regression'] - mean) / std
 print(df_result)
-#df_result.to_csv("sz_fmratio_z.csv")
+# df_result.to_csv("sz_fmratio_z.csv")
 
 df_result = df_result[["ID", "value_map"]]
 df_result = df_result.set_index(["ID"])
